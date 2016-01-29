18baf14645530504f8645a4ed06bc2e47facaec7
Markus Hanses <me@markushanses.de>
2016-01-29 12:17:36 +0100

    Tomcat AJP Connector configuration adjusted to run with apache2 vhost configuration.

--- src/main/java/com/jhipster/sample/config/TomcatAjpConfiguration.java
+++ src/main/java/com/jhipster/sample/config/TomcatAjpConfiguration.java
@@ -1,34 +1,34 @@
-package com.jhipster.sample.config;
-
-import org.apache.catalina.connector.Connector;
-import org.apache.catalina.valves.RemoteIpValve;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class TomcatAjpConfiguration {
-
-    @Bean
-    public EmbeddedServletContainerFactory servletContainer() {
-        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
-        tomcat.addAdditionalTomcatConnectors(createConnector());
-        tomcat.addContextValves(createRemoteIpValves());
-        return tomcat;
-    }
-
-    private RemoteIpValve createRemoteIpValves() {
-        RemoteIpValve remoteIpValve = new RemoteIpValve();
-        remoteIpValve.setRemoteIpHeader("x-forwarded-for");
-        remoteIpValve.setProtocolHeader("x-forwarded-protocol");
-        return remoteIpValve;
-    }
-
-    private static Connector createConnector() {
-        Connector connector = new Connector("AJP/1.3");
-        connector.setPort(8009);
-        return connector;
-    }
-
-}
+//package com.jhipster.sample.config;
+//
+//import org.apache.catalina.connector.Connector;
+//import org.apache.catalina.valves.RemoteIpValve;
+//import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+//import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class TomcatAjpConfiguration {
+//
+//    @Bean
+//    public EmbeddedServletContainerFactory servletContainer() {
+//        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+//        tomcat.addAdditionalTomcatConnectors(createConnector());
+//        tomcat.addContextValves(createRemoteIpValves());
+//        return tomcat;
+//    }
+//
+//    private RemoteIpValve createRemoteIpValves() {
+//        RemoteIpValve remoteIpValve = new RemoteIpValve();
+//        remoteIpValve.setRemoteIpHeader("x-forwarded-for");
+//        remoteIpValve.setProtocolHeader("x-forwarded-protocol");
+//        return remoteIpValve;
+//    }
+//
+//    private static Connector createConnector() {
+//        Connector connector = new Connector("AJP/1.3");
+//        connector.setPort(8009);
+//        return connector;
+//    }
+//
+//}
